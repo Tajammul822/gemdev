735933a95130eaa0c172c1c5536cef2a5f9e34e6
Tajammul822 <tajammulhussain822@gmail.com>
2025-08-21 17:28:44 +0500

    login route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'login_form'])->name('login');
